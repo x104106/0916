6a13d20e3400c01a5b17198997509af84f56e2d8
x104106 <70998530+x104106@users.noreply.github.com>
2020-09-16 13:48:51 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    資料 = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if ("F".compare(資料) == 0) {
+        basic.showString("F")
+    } else if ("R".compare(資料) == 0) {
+        basic.showString("R")
+    } else if ("L".compare(資料) == 0) {
+        basic.showString("L")
+    } else if ("B".compare(資料) == 0) {
+        basic.showString("B")
+    } else {
+        if ("S".compare(資料) == 0) {
+            basic.showString("S")
+        }
+    }
+})
+let 資料 = ""
+basic.showString("we")
+bluetooth.startTemperatureService()
+bluetooth.startUartService()
+資料 = ""
